3033c2c651522d1a8bbe87c025ce6891c40e7793
leihaisej <lei.school.io@gmail.com>
2025-05-25 22:11:34 +0800

    Initial Commit

---
+++ src/test/java/com/example/integprog/IntegprogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.integprog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegprogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
